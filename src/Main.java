293c3b0903012548ce8dc318f6d018a6915d08c7
dennis <dennis.stanislav@gmail.com>
2023-03-28 11:44:03 +0300

    have done a simple task to get a newest car from ArrayList

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.awt.*;
+import java.awt.List;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws Exception
+    {
+        LocalDate date = LocalDate.of(2021, 12, 21);
+        LocalDate date2 = LocalDate.of(2020, 12, 21);
+        LocalDate date3 = LocalDate.of(2019, 12, 21);
+
+        Car porshe =new Car(date);
+        Car lada =new Car(date2);
+        Car BWM =new Car(date3);
+       ArrayList<Car> cars=new ArrayList<>();
+
+        Car newestCar = cars.stream()
+                .max(Comparator.comparing(car -> car.getCreateDate()))
+                .orElse(null);
+
+
+    }
+
+}
+
+
+
+
+class Car
+{
+   private LocalDate createDate;
+
+    public Car(LocalDate createDate) {
+        this.createDate = createDate;
+    }
+
+    public LocalDate getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDate createDate) {
+        this.createDate = createDate;
+    }
+}
+
+
+
